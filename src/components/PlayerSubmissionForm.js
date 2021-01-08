78db63964d7748504307f3cc9aa2e2011ba3467a
Kalkidan Ashenafi <kalkidanashenafi@gmail.com>
2021-01-07 22:04:26 -0800

    Built out PlayerSubmissionForm + got started on Recent Submission

--- src/components/PlayerSubmissionForm.js
+++ src/components/PlayerSubmissionForm.js
@@ -4,20 +4,57 @@ import PropTypes from 'prop-types';
 import './PlayerSubmissionForm.css';
 
 const PlayerSubmissionForm = () => {
+  const PlayerSubmissionForm = (props) => {
+    const [wordFields, setWordFields] = useState({
+      adjOne: '',
+      nounOne: '',
+      adverb: '',
+      verb: '',
+      adjTwo: '',
+      nounTwo: ''
+    })
+  
+    const onInputChange = (event) => {
+      console.log(`Changing field ${ event.target.name } to ${ event.target.value }`);
+      const newWordFields = {
+        ...wordFields,
+      }
+      newWordFields[event.target.name] = event.target.value;
+      setWordFields(newWordFields);
+    }
+  
+    const onFormSubmit = (event) => {
+      event.preventDefault(); 
+  
+      props.submitPlayerLine(wordFields);
+  
+      setWordFields({
+        adjOne: '',
+        nounOne: '',
+        adverb: '',
+        verb: '',
+        adjTwo: '',
+        nounTwo: ''
+      });
+    };
+
   return (
     <div className="PlayerSubmissionForm">
       <h3>Player Submission Form for Player #{  }</h3>
 
-      <form className="PlayerSubmissionForm__form" >
+      <form className="PlayerSubmissionForm__form" onSubmit={onFormSubmit}>
 
         <div className="PlayerSubmissionForm__poem-inputs">
 
           {
             // Put your form inputs here... We've put in one below as an example
           }
-          <input
-            placeholder="hm..."
-            type="text" />
+          <input type="text" placeholder="adjective" name="adjOne" value={wordFields.adjOne} onChange={onInputChange}/>
+          <input type="text" placeholder="noun" name="nounOne" value={wordFields.nounOne} onChange={onInputChange}/>
+          <input type="text" placeholder="adverb" name="adverb" value={wordFields.adverb} onChange={onInputChange}/>
+          <input type="text" placeholder="verb" name="verb" value={wordFields.verb} onChange={onInputChange}/>
+          <input type="text" placeholder="adjective" name="adjTwo" value={wordFields.adjTwo} onChange={onInputChange}/>
+          <input type="text" placeholder="noun" name="nounTwo" value={wordFields.nounTwo} onChange={onInputChange}/>
 
         </div>
 
--- src/components/RecentSubmission.js
+++ src/components/RecentSubmission.js
@@ -1,8 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import './RecentSubmission.css';
+import PlayerSubmissionForm from './PlayerSubmissionForm';
 
 const RecentSubmission = (props) => {
+  const compiledSubmissions = props.submissions.map((submission) => {
+    return(
+      <PlayerSubmissionForm 
+      adjOne={submission.adjOne} 
+      nounOne={submission.nounOne} 
+      adverb={submission.adverb} 
+      verb={submission.verb} 
+      adjTwo={submission.adjTwo} 
+      nounTwo={submission.nounTwo} />
+    )
+  });
+
   return (
     <div className="RecentSubmission">
       <h3>The Most Recent Submission</h3>
