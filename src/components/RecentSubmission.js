384bf904471556c8139273c33850ae0423541188
Kalkidan Ashenafi <kalkidanashenafi@gmail.com>
2021-01-08 03:24:46 -0800

    all tests passing

--- src/components/RecentSubmission.js
+++ src/components/RecentSubmission.js
@@ -1,26 +1,14 @@
+
 import React from 'react';
 import PropTypes from 'prop-types';
 import './RecentSubmission.css';
-import PlayerSubmissionForm from './PlayerSubmissionForm';
 
 const RecentSubmission = (props) => {
-  const compiledSubmissions = props.submissions.map((submission) => {
-    return(
-      <PlayerSubmissionForm 
-      adjOne={submission.adjOne} 
-      nounOne={submission.nounOne} 
-      adverb={submission.adverb} 
-      verb={submission.verb} 
-      adjTwo={submission.adjTwo} 
-      nounTwo={submission.nounTwo} />
-    )
-  });
-
   return (
     <div className="RecentSubmission">
       <h3>The Most Recent Submission</h3>
       <p className="RecentSubmission__submission">
-        {props.newLine}
+        { props.submission }
       </p>
     </div>
   );
